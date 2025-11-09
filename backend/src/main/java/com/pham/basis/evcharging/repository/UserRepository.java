3575b391e874f399d635df085e31fbc373587025
Sharks-s <tranphipham.hvt@gmail.com>
2025-11-09 14:26:24 +0700

    add cash method

--- backend/src/main/java/com/pham/basis/evcharging/repository/UserRepository.java
+++ backend/src/main/java/com/pham/basis/evcharging/repository/UserRepository.java
@@ -6,18 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-//interface → không cần viết code xử lý, Spring Data JPA sẽ tự động generate implementation khi chạy.
-//Nếu cần custom query phức tạp hơn, có thể dùng @Query với JPQL hoặc SQL.
-// Kế thừa JpaRepository để thao tác với DB thông qua Entity User
-// User: entity class (tương ứng bảng User)
-// Integer: kiểu dữ liệu của Primary Key (id)
+
 @Repository
-// @Repository: đánh dấu class là tầng truy xuất dữ liệu (DAO)
-// Không cần dùng khi kế thừa JpaRepository, vì Spring Data JPA đã tự thêm.
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findUserByUsername(String username);
-// JpaRepository đã có sẵn các method cơ bản:
-// findAll(), findById(), save(), deleteById()...
     User findByEmail(String email);
     User findByPhone(String phone);
     boolean existsByEmail(String email);
--- backend/src/main/java/com/pham/basis/evcharging/repository/VerificationTokenRepository.java
+++ backend/src/main/java/com/pham/basis/evcharging/repository/VerificationTokenRepository.java
@@ -10,7 +10,5 @@ import java.util.Optional;
 @Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Integer> {
     Optional<VerificationToken> findByToken(String token);
-    //Optional là một container object có thể chứa giá trị hoặc rỗng (null)
-    //Giúp tránh NullPointerException
     void deleteByUser(User user);
 }
